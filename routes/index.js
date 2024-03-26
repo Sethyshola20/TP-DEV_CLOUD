293e367bba88a17720aaa1a43a3bfc3ce296425b
EMehraein <emehraein@medinplus.com>
2024-03-26 14:57:06 +0100

    Route get Data Json et connexion BDD

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,25 @@ Routes.get("/", (req, res) => {
   });
 });
 
+Routes.get("/data", async (req, res) => {
+  try {
+    // Chemin vers le fichier JSON
+    const filePath = path.join(__dirname, "data.json");
+
+    // Lecture du fichier JSON
+    const jsonData = await fs.readFile(filePath, "utf-8");
+
+    // Conversion de la chaîne JSON en objet JavaScript
+    const data = JSON.parse(jsonData);
+
+    // Envoi de la réponse avec les données JSON
+    return res.json(data);
+  } catch (error) {
+    console.error("Erreur lors de la lecture du fichier JSON :", error);
+    return res.status(500).json({
+      message: "Erreur lors de la récupération des données",
+    });
+  }
+});
+
 module.exports = Routes;
