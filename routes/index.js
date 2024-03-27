8eaa1c56cd4f300e4efc092d62793b6c3c797ce1
EMehraein <emehraein@medinplus.com>
2024-03-27 11:52:25 +0100

    route push_json)

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 const Routes = Router();
 
 Routes.get("/", (req, res) => {
-  console.log("GG");
   return res.json({
     message: "Hello World",
   });
@@ -11,7 +10,7 @@ Routes.get("/", (req, res) => {
 Routes.get("/data", async (req, res) => {
   try {
     // Chemin vers le fichier JSON
-    const filePath = path.join(__dirname, "data.json");
+    const filePath = path.join("data.json");
 
     // Lecture du fichier JSON
     const jsonData = await fs.readFile(filePath, "utf-8");
@@ -26,6 +25,18 @@ Routes.get("/data", async (req, res) => {
     return res.status(500).json({
       message: "Erreur lors de la récupération des données",
     });
+  }
+});
+
+Routes.post("/push_json", (req, res) => {
+  try {
+      // Récupération du JSON envoyé par l'utilisateur
+      const data = fs.readFile("data.json", "utf-8");
+
+      // Traitement du JSON (ici, nous le renvoyons simplement en réponse)
+      res.status(200).json({ message: 'JSON reçu avec succès', data });
+  } catch (error) {
+      res.status(400).json({ message: 'Erreur lors du traitement du JSON', error: error.message });
   }
 });
 
